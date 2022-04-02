5f94163a839ea25e765cda602190428b2d408088
Taylan Yildiz <97751293+taylanyild@users.noreply.github.com>
2022-04-02 12:13:51 +0200

    Delete shipment.py

--- shipment.py
+++
@@ -1,8 +0,0 @@
-class Shipment:
-    """A sample Shipment class"""
-
-    def __init__(self, name, name_recipient, destination, weight):
-        self.name = name 
-        self.name_recipient = name_recipient 
-        self.destination = destination 
-        self.weight = weight
